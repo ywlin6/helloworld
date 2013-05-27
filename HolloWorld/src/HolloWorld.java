999e171a075e0e41b651d735c76238c9e827e9bb
ywlin6 <ywlin6@yahoo.com>
2013-05-27 18:19:26 -0500

    this is the first project that i push to git

---
+++ HolloWorld/src/HolloWorld.java
@@ -0,0 +1,14 @@
+
+public class HolloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println ("Hollo World");
+		System.out.println ("On my machine");
+		System.out.println ("First project");
+		System.out.println ("Bye now!!!");
+	}
+
+}
